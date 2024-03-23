63c3edcff8609e8247aaf5056d03f57453b68861
nanjizal <nanjizal@users.noreply.github.com>
2024-03-23 11:44:13 +0000

    Add files via upload
    add PeoteTest files

---
+++ src/Main.hx
@@ -0,0 +1,25 @@
+package;
+
+import pi_xy.imageAbstracts.PeoteTexture;
+import haxe.CallStack;
+
+import lime.app.Application;
+import lime.ui.Window;
+
+import peote.view.*;
+
+class Main extends Application {
+    override function onWindowCreate(): Void {
+        switch( window.context.type ){
+            case WEBGL, OPENGL, OPENGLES:
+                try startSample( window )
+                catch (_) trace(CallStack.toString(CallStack.exceptionStack()), _);
+            default: throw("Sorry, only works with OpenGL.");
+        }
+    }
+    public function startSample( window: lime.ui.Window ){
+        new pi_xy.application.PeoteTest( window );
+    }
+    override function onPreloadComplete():Void {}
+    override function update(deltaTime:Int):Void {}
+}
